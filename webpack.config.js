d1842f4ad673db40503a4cd369ad6a0b0a552e64
SOUnit <workbookwork7@gmail.com>
2021-02-17 08:34:07 +0900

    added build config to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,19 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/,
+      },
+    ],
+  },
 };
