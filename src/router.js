13f888aab309f4fd9c01df5f2a605a11396f3e51
SOUnit <workbookwork7@gmail.com>
2021-02-20 18:29:24 +0900

    added splite code to react

--- src/router.js
+++ src/router.js
@@ -3,20 +3,63 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import Home from './components/Home';
 import ArtistMain from './components/artists/ArtistMain';
-import ArtistDetail from './components/artists/ArtistDetail';
-import ArtistCreate from './components/artists/ArtistCreate';
-import ArtistEdit from './components/artists/ArtistEdit';
+
+// plain routes component pattern
+const componentRoutes = {
+  component: Home,
+  path: '/',
+  indexRoute: { component: ArtistMain },
+  childRoutes: [
+    {
+      path: 'artists/new',
+      // asynchroness load component
+      getComponent(location, cb) {
+        // for webpack separate file load
+        System.import('./components/artists/ArtistCreate').then((module) =>
+          // syntax: cb(error, module)
+          cb(null, module.default)
+        );
+      },
+    },
+    {
+      path: 'artists/:id',
+      // asynchroness load component
+      getComponent(location, cb) {
+        // for webpack separate file load
+        System.import('./components/artists/ArtistDetail').then((module) =>
+          // syntax: cb(error, module)
+          cb(null, module.default)
+        );
+      },
+    },
+    {
+      path: 'artists/:id/edit',
+      // asynchroness load component
+      getComponent(location, cb) {
+        // for webpack separate file load
+        System.import('./components/artists/ArtistEdit').then((module) =>
+          // syntax: cb(error, module)
+          cb(null, module.default)
+        );
+      },
+    },
+  ],
+};
 
 const Routes = () => {
   return (
-    <Router history={hashHistory}>
-      <Route path="/" component={Home}>
-        <IndexRoute component={ArtistMain} />
-        <Route path="artists/new" component={ArtistCreate} />
-        <Route path="artists/:id" component={ArtistDetail} />
-        <Route path="artists/:id/edit" component={ArtistEdit} />
-      </Route>
-    </Router>
+    // JSX syntax for react
+    // <Router history={hashHistory}>
+    //   <Route path='/' component={Home}>
+    //     <IndexRoute component={ArtistMain} />
+    //     <Route path='artists/new' component={ArtistCreate} />
+    //     <Route path='artists/:id' component={ArtistDetail} />
+    //     <Route path='artists/:id/edit' component={ArtistEdit} />
+    //   </Route>
+    // </Router>
+
+    // plain route component for webpack
+    <Router history={hashHistory} routes={componentRoutes} />
   );
 };
 
